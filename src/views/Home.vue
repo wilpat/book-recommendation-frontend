f33032765ccb6036588edf0772c1a73f7e26aff4
William Okafor <wilpat456@gmail.com>
2019-10-03 02:34:57 +0100

    Connecting front end to laravel and fixes graphql error.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin" @click="selectCategory(category.id)">
+            {{ category.id }}. {{ category.name }}
+          </a>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    <ApolloQuery :query="require('@/graphql/queries/Books.gql')" v-if="selectedCategory == 0">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div v-for="book of data.books" :key="book.id">
+            {{ book.id }}. {{ book.title }}
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    
+    <ApolloQuery :query="require('@/graphql/queries/Category.gql')" :variables="{id: selectedCategory}" v-else>
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div v-for="book of data.category.books" :key="book.id">
+            {{ book.id }}. {{ book.title }}
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data () {
+    return {
+      selectedCategory: 0
+    }
+  },
+
+  methods: {
+    selectCategory(id) {
+      this.selectedCategory = id;
+    }
   }
 }
 </script>
+
+<style>
+  .link-margin{
+    margin-right: 24px;
+  }
+</style>
