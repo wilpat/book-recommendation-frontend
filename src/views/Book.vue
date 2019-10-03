e6fd8773266794c4b2bf9c9373439554ce08fd34
William Okafor <wilpat456@gmail.com>
2019-10-03 20:14:11 +0100

    Fixes rendering bug, More frontend work.

---
+++ src/views/Book.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    
+    <ApolloQuery :query="require('@/graphql/queries/Book.gql')" :variables="{id: $route.params.id}">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div v-if="data.book">
+              <div>{{ data.book.title }}</div>
+              <div>{{ data.book.author }}</div>
+              <img :src="`${data.book.image}`" alt="">
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,64 @@
 <template>
   <div class="home">
-    <ApolloQuery :query="require('@/graphql/queries/Categories.gql')">
+    <ApolloQuery :query="CategoriesQuery">
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin" @click="selectCategory(category.id)">
+          <a href="#" @click.prevent="selectCategory(0)" class="link-margin">All</a>
+          <a href="#" @click.prevent="selectCategory(-1)" class="link-margin">Featured</a>
+          <a href="#" v-for="category of data.categories" :key="category.id" class="link-margin" @click.prevent="selectCategory(category.id)">
             {{ category.id }}. {{ category.name }}
           </a>
         </div>
       </template>
     </ApolloQuery>
 
-    <ApolloQuery :query="require('@/graphql/queries/Books.gql')" v-if="selectedCategory == 0">
+    <ApolloQuery v-if="selectedCategory == 0" :query="query">
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <div v-for="book of data.books" :key="book.id">
-            {{ book.id }}. {{ book.title }}
+          <div v-if="data.books">
+            <div v-for="book of data.books" :key="book.id">
+              <router-link :to="{name:'books', params:{'id': book.id}}"> 
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`${book.image}`" alt="">
+            </div>
           </div>
         </div>
       </template>
     </ApolloQuery>
 
-    
-    <ApolloQuery :query="require('@/graphql/queries/Category.gql')" :variables="{id: selectedCategory}" v-else>
+    <ApolloQuery v-if="selectedCategory == -1" :query="query" :variables="{featured: true}">
       <template slot-scope="{ result: { data, loading }, isLoading }">
         <div v-if="isLoading">Loading...</div>
         <div v-else>
-          <div v-for="book of data.category.books" :key="book.id">
-            {{ book.id }}. {{ book.title }}
+          <div v-if="data.booksByFeatured">
+            <div v-for="book of data.booksByFeatured" :key="book.id">
+              <router-link :to="{name:'books', params:{'id': book.id}}"> 
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`${book.image}`" alt="">
+            </div>
+          </div>
+        </div>
+      </template>
+    </ApolloQuery>
+
+    <ApolloQuery v-if="selectedCategory > 0" :query="query" :variables="{id: selectedCategory}">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
+          <div v-if="data.category">
+            <div v-for="book of data.category.books" :key="book.id">
+              <router-link :to="{name:'books', params:{'id': book.id}}"> 
+                {{ book.id }}. {{ book.title }}
+              </router-link>
+              <div>{{ book.author }}</div>
+              <img :src="`${book.image}`" alt="">
+            </div>
           </div>
         </div>
       </template>
@@ -40,7 +70,10 @@
 
 <script>
 // @ is an alias to /src
-
+import BooksQuery from "@/graphql/queries/Books.gql";
+import CategoriesQuery from "@/graphql/queries/Categories.gql";
+import CategoryQuery from "@/graphql/queries/Category.gql";
+import BooksByFeaturedQuery from "@/graphql/queries/BooksByFeatured.gql";
 export default {
   name: 'home',
   components: {
@@ -48,14 +81,27 @@ export default {
   },
   data () {
     return {
-      selectedCategory: 0
+      selectedCategory: 0,
+      query:BooksQuery,
+      BooksQuery,
+      CategoriesQuery,
+      CategoryQuery,
+      BooksByFeaturedQuery
     }
   },
 
   methods: {
-    selectCategory(id) {
-      this.selectedCategory = id;
-    }
+    selectCategory(category) {
+      if(category === 0){
+        this.query = BooksQuery;
+      } else if(category === -1) {
+        this.query = BooksByFeaturedQuery;
+      } else{
+        this.query = CategoryQuery;
+      }
+      this.selectedCategory = parseInt(category);
+    },
+    
   }
 }
 </script>
